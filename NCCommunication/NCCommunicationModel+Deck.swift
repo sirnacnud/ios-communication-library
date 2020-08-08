82f664975106c1efd61b68dee90e2c0467609a82
Zac Gibson <zgibson@live.com>
2020-08-07 19:11:57 -0500

    OPTIONALS ARE THE FUTURE

--- NCCommunication/NCCommunicationModel+Deck.swift
+++ NCCommunication/NCCommunicationModel+Deck.swift
@@ -9,16 +9,16 @@ public struct NCCommunicationCards: Codable {
     public var stackId: Int = 0
     public var type: String = ""
     public var lastModified: Int = 0
-    public var lastEditor: String = ""
+    public var lastEditor: String?
     public var createdAt: Int = 0
     public var labels: [NCCommunicationLabels] = []
     public var assignedUsers: [NCCommunicationDeckUsers] = []
-    public var attachments: String = ""
+    public var attachments: String?
     public var attachmentCount: Int = 0
     public var owner: NCCommunicationDeckUsers
     public var order: Int = 0
     public var archived: Bool = false
-    public var dueDate: String = ""
+    public var dueDate: String?
     public var deletedAt: Int = 0
     public var commentsUnread: Int = 0
     public var id: Int = 0
