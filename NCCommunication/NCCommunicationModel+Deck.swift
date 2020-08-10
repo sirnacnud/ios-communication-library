0074f37bda11c614900e9b357a317d5b5824f49b
Zac Gibson <zgibson@live.com>
2020-08-09 19:42:09 -0500

    this class actually works haha

--- NCCommunication/NCCommunicationModel+Deck.swift
+++ NCCommunication/NCCommunicationModel+Deck.swift
@@ -3,9 +3,6 @@
 
 import Foundation
 
-// Created for iOS Deck in 2020
-// Using Swift 5.0
-
 public final class NCCommunicationDeckCards: NSObject, NSItemProviderReading, NSItemProviderWriting, Codable, Identifiable {
     public static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> NCCommunicationDeckCards {
         let decoder = JSONDecoder()
@@ -26,7 +23,7 @@ public final class NCCommunicationDeckCards: NSObject, NSItemProviderReading, NS
         } catch {
             completionHandler(nil, error)
         }
-
+        
         return progress
         
     }
@@ -44,7 +41,7 @@ public final class NCCommunicationDeckCards: NSObject, NSItemProviderReading, NS
     public var createdAt: Int = 0
     public var labels: [NCCommunicationDeckLabels] = []
     public var assignedUsers: [NCCommunicationDeckUsers] = []
-    public var attachments: String?
+    public var attachments: String? = nil
     public var attachmentCount: Int = 0
     public var owner: NCCommunicationDeckUsers
     public var order: Int = 0
@@ -54,56 +51,29 @@ public final class NCCommunicationDeckCards: NSObject, NSItemProviderReading, NS
     public var commentsUnread: Int = 0
     public var id: Int = 0
     public var overdue: Int = 0
-
-    init(_ title: String, _ description: String, _ stackId: Int, _ type: String, _ lastModified: Int, _ lastEditor: String?, _ createdAt: Int, _ labels: [NCCommunicationDeckLabels], _ assignedUsers: [NCCommunicationDeckUsers], _ attachments: String?, attachmentCount: Int, owner: NCCommunicationDeckUsers, _ order: Int, _ archived: Bool, _ duedate: String?, _ deletedAt: Int, _ commentsUnread: Int, _ id: Int, _ overdue: Int) {
-        self.title = title
-        self.desc = description
-        self.stackId = stackId
-        self.type = type
-        self.lastModified = lastModified
-        self.lastEditor = lastEditor
-        self.createdAt = createdAt
-        self.labels = labels
-        self.assignedUsers = assignedUsers
-        self.attachments = attachments
-        self.attachmentCount = attachmentCount
-        self.owner = owner
-        self.archived = archived
-        self.duedate = duedate
-        self.deletedAt = deletedAt
-        self.commentsUnread = commentsUnread
-        self.id = id
-        self.overdue = overdue
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case desc = "description"
+        case stackId
+        case type
+        case lastModified
+        case lastEditor
+        case createdAt
+        case labels
+        case assignedUsers
+        case attachments
+        case attachmentCount
+        case owner
+        case order
+        case archived
+        case duedate
+        case deletedAt
+        case commentsUnread
+        case id
+        case overdue
     }
 }
-
-
-//public struct NCCommunicationDeckCards: Codable, Identifiable {
-//    public var title: String = ""
-//    public var description: String = ""
-//    public var stackId: Int = 0
-//    public var type: String = ""
-//    public var lastModified: Int = 0
-//    public var lastEditor: String?
-//    public var createdAt: Int = 0
-//    public var labels: [NCCommunicationDeckLabels] = []
-//    public var assignedUsers: [NCCommunicationDeckUsers] = []
-//    public var attachments: String?
-//    public var attachmentCount: Int = 0
-//    public struct owner: Codable {
-//        public var primaryKey: String = ""
-//        public var uid: String = ""
-//        public var displayname: String = ""
-//        public var type: Int = 0
-//    }
-//    public var order: Int = 0
-//    public var archived: Bool = false
-//    public var duedate: String?
-//    public var deletedAt: Int = 0
-//    public var commentsUnread: Int = 0
-//    public var id: Int = 0
-//    public var overdue: Int = 0
-//}
 
 public struct NCCommunicationDeckStacks: Codable, Identifiable {
     public var title: String = ""
