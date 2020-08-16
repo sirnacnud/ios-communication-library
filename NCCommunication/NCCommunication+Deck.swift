3c28ab4f0edb33cf87f67d2d2bfce631f536caad
Zac Gibson <zgibson@live.com>
2020-08-15 23:44:54 -0500

    Add move card stuff

--- NCCommunication/NCCommunication+Deck.swift
+++ NCCommunication/NCCommunication+Deck.swift
@@ -3,6 +3,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 extension NCCommunication {
     public func getBoards(customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ boards: [NCCommunicationDeckBoards]?, _ errorCode: Int, _ errorDescription: String) -> Void) {
@@ -84,144 +85,188 @@ extension NCCommunication {
         }
     }
     
-    //    @objc public func putComments(fileId: String, message: String, customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ errorCode: Int, _ errorDescription: String) -> Void) {
-    //
-    //        let account = NCCommunicationCommon.shared.account
-    //        let serverUrlEndpoint = NCCommunicationCommon.shared.url + "/" + NCCommunicationCommon.shared.davRoot + "/comments/files/" + fileId
-    //
-    //        guard let url = NCCommunicationCommon.shared.encodeStringToUrl(serverUrlEndpoint) else {
-    //            completionHandler(account, NSURLErrorBadURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
-    //            return
-    //        }
-    //
-    //        let method = HTTPMethod(rawValue: "POST")
-    //
-    //        var headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
-    //        headers.update(.contentType("application/json"))
-    //
-    //        var urlRequest: URLRequest
-    //        do {
-    //            try urlRequest = URLRequest(url: url, method: method, headers: headers)
-    //            let parameters = "{\"actorType\":\"users\",\"verb\":\"comment\",\"message\":\"" + message + "\"}"
-    //            urlRequest.httpBody = parameters.data(using: .utf8)
-    //        } catch {
-    //            completionHandler(account, error._code, error.localizedDescription)
-    //            return
-    //        }
-    //
-    //        sessionManager.request(urlRequest).validate(statusCode: 200..<300).response { (response) in
-    //            debugPrint(response)
-    //
-    //            switch response.result {
-    //            case .failure(let error):
-    //                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
-    //                completionHandler(account, error.errorCode, error.description ?? "")
-    //            case .success( _):
-    //                completionHandler(account, 0, "")
-    //            }
-    //        }
-    //    }
-    //
-    //    @objc public func updateComments(fileId: String, messageId: String, message: String, customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ errorCode: Int, _ errorDescription: String) -> Void) {
-    //
-    //        let account = NCCommunicationCommon.shared.account
-    //        let serverUrlEndpoint = NCCommunicationCommon.shared.url + "/" + NCCommunicationCommon.shared.davRoot + "/comments/files/" + fileId + "/" + messageId
-    //
-    //        guard let url = NCCommunicationCommon.shared.encodeStringToUrl(serverUrlEndpoint) else {
-    //            completionHandler(account, NSURLErrorBadURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
-    //            return
-    //        }
-    //
-    //        let method = HTTPMethod(rawValue: "PROPPATCH")
-    //
-    //        var headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
-    //        headers.update(.contentType("application/xml"))
-    //
-    //        var urlRequest: URLRequest
-    //        do {
-    //            try urlRequest = URLRequest(url: url, method: method, headers: headers)
-    //            let parameters = String(format: NCDataFileXML().requestBodyCommentsUpdate, message)
-    //            urlRequest.httpBody = parameters.data(using: .utf8)
-    //        } catch {
-    //            completionHandler(account, error._code, error.localizedDescription)
-    //            return
-    //        }
-    //
-    //        sessionManager.request(urlRequest).validate(statusCode: 200..<300).response { (response) in
-    //            debugPrint(response)
-    //
-    //            switch response.result {
-    //            case .failure(let error):
-    //                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
-    //                completionHandler(account, error.errorCode, error.description ?? "")
-    //            case .success( _):
-    //                completionHandler(account, 0, "")
-    //            }
-    //        }
-    //    }
-    //
-    //    @objc public func deleteComments(fileId: String, messageId: String, customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ errorCode: Int, _ errorDescription: String) -> Void) {
-    //
-    //        let account = NCCommunicationCommon.shared.account
-    //        let serverUrlEndpoint = NCCommunicationCommon.shared.url + "/" + NCCommunicationCommon.shared.davRoot + "/comments/files/" + fileId + "/" + messageId
-    //
-    //        guard let url = NCCommunicationCommon.shared.encodeStringToUrl(serverUrlEndpoint) else {
-    //            completionHandler(account, NSURLErrorBadURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
-    //            return
-    //        }
-    //
-    //        let method = HTTPMethod(rawValue: "DELETE")
-    //
-    //        let headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
-    //
-    //        sessionManager.request(url, method: method, parameters: nil, encoding: URLEncoding.default, headers: headers, interceptor: nil).validate(statusCode: 200..<300).response { (response) in
-    //            debugPrint(response)
-    //
-    //            switch response.result {
-    //            case .failure(let error):
-    //                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
-    //                completionHandler(account, error.errorCode, error.description ?? "")
-    //            case .success( _):
-    //                completionHandler(account, 0, "")
-    //            }
-    //        }
-    //    }
-    //
-    //    @objc public func markAsReadComments(fileId: String, customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ errorCode: Int, _ errorDescription: String) -> Void) {
-    //
-    //        let account = NCCommunicationCommon.shared.account
-    //        let serverUrlEndpoint = NCCommunicationCommon.shared.url + "/" + NCCommunicationCommon.shared.davRoot + "/comments/files/" + fileId
-    //
-    //        guard let url = NCCommunicationCommon.shared.encodeStringToUrl(serverUrlEndpoint) else {
-    //            completionHandler(account, NSURLErrorBadURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
-    //            return
-    //        }
-    //
-    //        let method = HTTPMethod(rawValue: "PROPPATCH")
-    //
-    //        var headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
-    //        headers.update(.contentType("application/xml"))
-    //
-    //        var urlRequest: URLRequest
-    //        do {
-    //            try urlRequest = URLRequest(url: url, method: method, headers: headers)
-    //            let parameters = String(format: NCDataFileXML().requestBodyCommentsMarkAsRead)
-    //            urlRequest.httpBody = parameters.data(using: .utf8)
-    //        } catch {
-    //            completionHandler(account, error._code, error.localizedDescription)
-    //            return
-    //        }
-    //
-    //        sessionManager.request(urlRequest).validate(statusCode: 200..<300).response { (response) in
-    //            debugPrint(response)
-    //
-    //            switch response.result {
-    //            case .failure(let error):
-    //                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
-    //                completionHandler(account, error.errorCode, error.description ?? "")
-    //            case .success( _):
-    //                completionHandler(account, 0, "")
-    //            }
-    //        }
-    //    }
+    public func moveCard(boardID: Int, stackID: Int, cardID: Int, order: Int, newStackID: Int?, customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ card: NCCommunicationDeckCards?, _ errorCode: Int, _ errorDescription: String) -> Void) {
+        
+        let account = NCCommunicationCommon.shared.account
+        let endpoint = "/boards/" + String(boardID) + "/stacks/" + String(stackID) + "/cards/" + String(cardID) + "/reorder"
+        
+        guard let url = NCCommunicationCommon.shared.createStandardUrl(serverUrl: NCCommunicationCommon.shared.url, endpoint: endpoint) else {
+            completionHandler(account, nil, NSURLErrorBadURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
+            return
+        }
+        
+        let method = HTTPMethod(rawValue: "PUT")
+        
+        let headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
+        
+        var parameters = [
+            "order": order
+        ]
+        if newStackID != nil {
+            parameters["stackId"] = newStackID!
+        } else {
+            parameters["stackId"] = stackID
+        }
+        
+        sessionManager.request(url, method: method, parameters: parameters, encoding: URLEncoding.default, headers: headers, interceptor: nil).validate(statusCode: 200..<300).responseJSON {
+            (response) in
+            debugPrint(response)
+            
+            switch response.result {
+            case .failure(let error):
+                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
+                completionHandler(account, nil, error.errorCode, error.description ?? "")
+            case .success(let json):
+                let json = JSON(json)
+                
+                let statusCode = json["ocs"]["meta"]["statuscode"].int ?? NCCommunicationError().getInternalError()
+                if statusCode == 200 {
+                    completionHandler(account, nil, 0, "")
+                }  else {
+                    let errorDescription = json["ocs"]["meta"]["message"].string ?? NSLocalizedString("_invalid_data_format_", value: "Invalid data format", comment: "")
+                    completionHandler(account, nil, statusCode, errorDescription)
+                }
+        }
+    }
 }
+
+//    @objc public func putComments(fileId: String, message: String, customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ errorCode: Int, _ errorDescription: String) -> Void) {
+//
+//        let account = NCCommunicationCommon.shared.account
+//        let serverUrlEndpoint = NCCommunicationCommon.shared.url + "/" + NCCommunicationCommon.shared.davRoot + "/comments/files/" + fileId
+//
+//        guard let url = NCCommunicationCommon.shared.encodeStringToUrl(serverUrlEndpoint) else {
+//            completionHandler(account, NSURLErrorBadURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
+//            return
+//        }
+//
+//        let method = HTTPMethod(rawValue: "POST")
+//
+//        var headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
+//        headers.update(.contentType("application/json"))
+//
+//        var urlRequest: URLRequest
+//        do {
+//            try urlRequest = URLRequest(url: url, method: method, headers: headers)
+//            let parameters = "{\"actorType\":\"users\",\"verb\":\"comment\",\"message\":\"" + message + "\"}"
+//            urlRequest.httpBody = parameters.data(using: .utf8)
+//        } catch {
+//            completionHandler(account, error._code, error.localizedDescription)
+//            return
+//        }
+//
+//        sessionManager.request(urlRequest).validate(statusCode: 200..<300).response { (response) in
+//            debugPrint(response)
+//
+//            switch response.result {
+//            case .failure(let error):
+//                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
+//                completionHandler(account, error.errorCode, error.description ?? "")
+//            case .success( _):
+//                completionHandler(account, 0, "")
+//            }
+//        }
+//    }
+//
+//    @objc public func updateComments(fileId: String, messageId: String, message: String, customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ errorCode: Int, _ errorDescription: String) -> Void) {
+//
+//        let account = NCCommunicationCommon.shared.account
+//        let serverUrlEndpoint = NCCommunicationCommon.shared.url + "/" + NCCommunicationCommon.shared.davRoot + "/comments/files/" + fileId + "/" + messageId
+//
+//        guard let url = NCCommunicationCommon.shared.encodeStringToUrl(serverUrlEndpoint) else {
+//            completionHandler(account, NSURLErrorBadURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
+//            return
+//        }
+//
+//        let method = HTTPMethod(rawValue: "PROPPATCH")
+//
+//        var headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
+//        headers.update(.contentType("application/xml"))
+//
+//        var urlRequest: URLRequest
+//        do {
+//            try urlRequest = URLRequest(url: url, method: method, headers: headers)
+//            let parameters = String(format: NCDataFileXML().requestBodyCommentsUpdate, message)
+//            urlRequest.httpBody = parameters.data(using: .utf8)
+//        } catch {
+//            completionHandler(account, error._code, error.localizedDescription)
+//            return
+//        }
+//
+//        sessionManager.request(urlRequest).validate(statusCode: 200..<300).response { (response) in
+//            debugPrint(response)
+//
+//            switch response.result {
+//            case .failure(let error):
+//                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
+//                completionHandler(account, error.errorCode, error.description ?? "")
+//            case .success( _):
+//                completionHandler(account, 0, "")
+//            }
+//        }
+//    }
+//
+//    @objc public func deleteComments(fileId: String, messageId: String, customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ errorCode: Int, _ errorDescription: String) -> Void) {
+//
+//        let account = NCCommunicationCommon.shared.account
+//        let serverUrlEndpoint = NCCommunicationCommon.shared.url + "/" + NCCommunicationCommon.shared.davRoot + "/comments/files/" + fileId + "/" + messageId
+//
+//        guard let url = NCCommunicationCommon.shared.encodeStringToUrl(serverUrlEndpoint) else {
+//            completionHandler(account, NSURLErrorBadURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
+//            return
+//        }
+//
+//        let method = HTTPMethod(rawValue: "DELETE")
+//
+//        let headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
+//
+//        sessionManager.request(url, method: method, parameters: nil, encoding: URLEncoding.default, headers: headers, interceptor: nil).validate(statusCode: 200..<300).response { (response) in
+//            debugPrint(response)
+//
+//            switch response.result {
+//            case .failure(let error):
+//                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
+//                completionHandler(account, error.errorCode, error.description ?? "")
+//            case .success( _):
+//                completionHandler(account, 0, "")
+//            }
+//        }
+//    }
+//
+//    @objc public func markAsReadComments(fileId: String, customUserAgent: String? = nil, addCustomHeaders: [String: String]? = nil, completionHandler: @escaping (_ account: String, _ errorCode: Int, _ errorDescription: String) -> Void) {
+//
+//        let account = NCCommunicationCommon.shared.account
+//        let serverUrlEndpoint = NCCommunicationCommon.shared.url + "/" + NCCommunicationCommon.shared.davRoot + "/comments/files/" + fileId
+//
+//        guard let url = NCCommunicationCommon.shared.encodeStringToUrl(serverUrlEndpoint) else {
+//            completionHandler(account, NSURLErrorBadURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
+//            return
+//        }
+//
+//        let method = HTTPMethod(rawValue: "PROPPATCH")
+//
+//        var headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
+//        headers.update(.contentType("application/xml"))
+//
+//        var urlRequest: URLRequest
+//        do {
+//            try urlRequest = URLRequest(url: url, method: method, headers: headers)
+//            let parameters = String(format: NCDataFileXML().requestBodyCommentsMarkAsRead)
+//            urlRequest.httpBody = parameters.data(using: .utf8)
+//        } catch {
+//            completionHandler(account, error._code, error.localizedDescription)
+//            return
+//        }
+//
+//        sessionManager.request(urlRequest).validate(statusCode: 200..<300).response { (response) in
+//            debugPrint(response)
+//
+//            switch response.result {
+//            case .failure(let error):
+//                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
+//                completionHandler(account, error.errorCode, error.description ?? "")
+//            case .success( _):
+//                completionHandler(account, 0, "")
+//            }
+//        }
+//    }
